83971ed40607fa5e580e6adf6cf0bc49cfe4ffaf
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-01-18 09:05:09 +0100

    AnimatorController

--- Assets/Script/Boss_1/Boss_1_Animator.cs
+++ Assets/Script/Boss_1/Boss_1_Animator.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Boss_1_Animator : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayTargetAnimation(bool looping, float fadeTime)
+    {
+
+    }
 }
