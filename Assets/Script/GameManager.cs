5d026761c0dec8fa98a6f9ab89ff1f4013c990ba
VladimirChantitch <vladimir.chantitch@inetum.com>
2023-01-25 23:04:21 +0100

    SaveSystem

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,10 +8,41 @@ public class GameManager : MonoBehaviour
 {
    [SerializeField] UI_Manager ui_manager;
    [SerializeField] SaveManager saveManager;
+    UI_Manager.CurrentScrenn currentScrenn;
+
+    public bool Save;
+    public bool Load;
 
     private void Start()
     {
         ui_manager.SaveGame.AddListener(() => saveManager.SaveGame());
         ui_manager.LoadGame.AddListener(() => saveManager.LoadGame());
+        ui_manager.DeleteSaveFile.AddListener(() => saveManager.DestroySaveFile());
+
+        currentScrenn = ui_manager.GetCurrentScreen();
+
+        if (currentScrenn != UI_Manager.CurrentScrenn.MainMenu)
+        {
+            saveManager.LoadGame();
+        }
+    }
+
+    private void Update()
+    {
+        if (Save)
+        {
+            saveManager.SaveGame();
+        }
+
+        if (Load)
+        {
+            saveManager.LoadGame();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Save = false;
+        Load = false;
     }
 }
--- Assets/Script/SavingSystem/SaveManager.cs
+++ Assets/Script/SavingSystem/SaveManager.cs
@@ -1,20 +1,120 @@
+using player;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using Newtonsoft.Json;
+using Mono.Cecil.Cil;
+using System;
+using System.IO;
 
 namespace Boss.save
 {
     public class SaveManager : MonoBehaviour
     {
+        List<DTO> dTOs = new List<DTO>();
+
+
         public void LoadGame()
         {
-            Debug.Log("I'm doing something useful");
+            dTOs = new List<DTO>();
+            dTOs = GetDTOFromJsonString();
+
+            if (dTOs.Count == 0)
+            {
+                return;
+            }
+
+            List<UnityEngine.Object> savables = FindObjectsOfType<UnityEngine.Object>().Where(o => o is ISavable).ToList();
+            
+            foreach (DTO dto in dTOs)
+            {
+                switch (dto)
+                {
+                    case Player_DTO player_DTO:
+                        (savables.Where(s => s is PlayerManager).First() as ISavable).LoadData(player_DTO);
+                        break;
+                }
+            }
         }
 
         public void SaveGame()
         {
-            Debug.Log("Saving Game");
+            dTOs = new List<DTO>();
+
+            List<UnityEngine.Object> savables = FindObjectsOfType<UnityEngine.Object>().Where(o => o is ISavable).ToList();
+            savables.ForEach(s => dTOs.Add((s as ISavable).GetData()));
+
+            GenerateJsonString();
         }
+
+        private void GenerateJsonString()
+        {
+            string jsonStrng = "";
+            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Formatting = Formatting.Indented };
+
+            try
+            {
+                string path = Directory.GetCurrentDirectory();
+                Directory.CreateDirectory(path + @"\Saves");
+
+                string save_file_path = @$"{path}\Saves\SaveFile.json";
+                Debug.Log(save_file_path);
+                File.WriteAllText(save_file_path, JsonConvert.SerializeObject(dTOs, settings));
+
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+            }
+        }
+
+        private List<DTO> GetDTOFromJsonString()
+        {
+            List<DTO> saved_dtos = new List<DTO>();
+            JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Formatting = Formatting.Indented };
+
+            string path = Directory.GetCurrentDirectory();
+            path = Directory.GetDirectories(path).ToList().Where(dir_path => dir_path.Contains("Saves")).ToList().First();
+
+            if (path == null || !path.Contains("Saves"))
+            {
+                Debug.Log("<color=red> No save files Yet </color>");
+                return saved_dtos;
+
+            }
+            string save_path = Directory.GetFiles(path)[0];
+
+
+            try
+            {
+                saved_dtos = JsonConvert.DeserializeObject<List<DTO>>(File.ReadAllText(save_path), settings);
+                return saved_dtos;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message); 
+                return saved_dtos;
+            }
+        }
+
+        public void DestroySaveFile()
+        {
+            string path = Directory.GetCurrentDirectory();
+            path = Directory.GetDirectories(path).ToList().Where(dir_path => dir_path.Contains("Saves")).ToList().First();
+            Directory.Delete(path, true);
+        }
+    }
+
+    public interface ISavable
+    {
+        public abstract DTO GetData();
+        public abstract void LoadData(DTO dTO);
+    }
+
+    public class DTO
+    {
+
     }
 }
