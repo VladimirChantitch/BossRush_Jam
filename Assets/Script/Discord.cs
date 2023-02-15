0515b642b2c495238e04e16a965bfa9a3b8662b0
DorDeDor <zennxxx@gmail.com>
2023-02-15 23:30:21 +0200

    main menu upgrade

---
+++ Assets/Script/Discord.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discord : MonoBehaviour
+{
+public void GoDiscord()
+    {
+        Application.OpenURL("https://discord.gg/3TrkHb7f9E");
+    }
+}
